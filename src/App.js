42d54976b7514ecd9ae782b8b228f050ca2f9ca1
Harry Katsaris <mrkatsaris@gmail.com>
2023-03-15 10:34:05 +0100

    added missing className

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className='container'>
         <TodoList />
       </div>
     )
