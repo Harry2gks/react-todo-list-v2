fbae37b3dae23f0027f1bc0766a6bc5241a45d36
Harry Katsaris <mrkatsaris@gmail.com>
2023-03-16 12:57:34 +0100

    made a previous build for education purposes

--- src/Todo.js
+++ src/Todo.js
@@ -1,63 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export default class Todo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isEditing: false,
-      task: this.props.task,
-    };
-    this.handleRemove = this.handleRemove.bind(this);
-    this.toggleForm = this.toggleForm.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleToggle = this.handleToggle.bind(this);
-  }
-  handleRemove() {
-    this.props.removeTodo(this.props.id);
-  }
-  toggleForm() {
-    this.setState({ isEditing: !this.state.isEditing });
-  }
-  handleChange(evt) {
-    this.setState({
-      [evt.target.name]: evt.target.value,
-    });
-  }
-  handleSubmit(evt) {
-    evt.preventDefault();
-    // take the new task Data and pass it to the parent (todoList)
-    this.props.updateTodo(this.props.id, this.state.task);
-    this.setState({ isEditing: false });
-  }
-  handleToggle(evt) {
-    this.props.toggleTodo(this.props.id);
-  }
-  render() {
-    let edit;
-    if (this.state.isEditing) {
-      edit = (
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            value={this.state.task}
-            name="task"
-            onChange={this.handleChange}
-          />
-          <button>Save</button>
-        </form>
-      );
-    } else {
-      edit = (
-        <div>
-          <li className={this.props.completed ? "completed" : ""}
-          onClick={this.handleToggle}>
-            {this.props.task}</li>
-          <button onClick={this.toggleForm}>Edit</button>
-          <button onClick={this.handleRemove}>Delete</button>
-        </div>
-      );
+    constructor(props) {
+        super(props);
     }
-    return edit;
+  render() {
+    return (
+      <div>
+        <li>{this.props.task}</li>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    )
   }
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,63 +1,23 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
-import Form from "./Form";
 
 export default class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.state = { todos: [] };
-    this.create = this.create.bind(this);
-    this.remove = this.remove.bind(this);
-    this.update = this.update.bind(this);
-    this.toggleCompleted = this.toggleCompleted.bind(this);
-  }
-  create(newTodo) {
-    this.setState({
-      todos: [...this.state.todos, newTodo],
-    });
-  }
-  remove(id) {
-    this.setState({
-      todos: this.state.todos.filter((t) => t.id !==id),
-    });
-  }
-  update(id, updatedTask) {
-    const updatedTodos = this.state.todos.map((t) => {
-      if (t.id === id) {
-        return { ...t, task: updatedTask };
-      }
-      return t;
-    });
-    this.setState({ todos: updatedTodos });
-  }
-  toggleCompleted(id) {
-    const updatedTodos = this.state.todos.map((t) => {
-      if (t.id === id) {
-        return { ...t, completed: !t.completed };
-      }
-      return t;
-    });
-    this.setState({ todos: updatedTodos });
+    this.state = { todos: [task: "Feed the cat"]};
   }
   render() {
     const todos = this.state.todos.map((t) => {
-      return <Todo 
-      key={t.id} 
-      id={t.id} 
-      task={t.task} 
-      completed={t.completed}
-      removeTodo={this.remove}
-      updateTodo={this.update}
-      toggleTodo={this.toggleCompleted}
-      />;
+      return <Todo task={t.task} />;
     });
     return (
       <div>
         <h1> Simple React To Do List App</h1>
-        <Form createNewTodo={this.create}  />
+        <Form />
         <ul> {todos} </ul>
         <h5>Made with &hearts; by harry2gks</h5>
       </div>
     );
   }
 }
+
